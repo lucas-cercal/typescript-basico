b872b3c987b1f1a41261e5c8a3d3da17257cbdca
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-07 15:29:56 -0300

    feat: challenge concluded

---
+++ 1_intro/desafio.js
@@ -0,0 +1,5 @@
+function sumNumber(num1, num2) {
+    var result = num1 + num2;
+    console.log("The sum of ".concat(num1, " and ").concat(num2, " is ") + result);
+}
+sumNumber(1, 2);
---
+++ 1_intro/desafio.ts
@@ -0,0 +1,7 @@
+
+function sumNumber(num1: number, num2:  number) {
+  const result = num1 + num2
+  console.log(`The sum of ${num1} and ${num2} is ` + result)
+}
+
+sumNumber(1, 2)
