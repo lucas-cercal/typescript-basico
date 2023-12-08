7d716ba2598e689197345244ad346164c8d20f3a
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-08 17:14:59 -0300

    feat: add instanceof

--- 4_narrowing/index.js
+++ 4_narrowing/index.js
@@ -1,4 +1,19 @@
 // 1 - type guard
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 function sum(a, b) {
     if (typeof a === 'string' && typeof b === 'string')
         console.log(parseFloat(a) + parseFloat(b));
@@ -28,3 +43,29 @@ function operations(arr, operation) {
 operations([1, 2, 3], 'sum');
 operations([1, 2, 3], 'multiply');
 operations([1, 2, 3]);
+// instance of
+var User = /** @class */ (function () {
+    function User(name) {
+        this.name = name;
+    }
+    return User;
+}());
+var SuperUser = /** @class */ (function (_super) {
+    __extends(SuperUser, _super);
+    function SuperUser(name) {
+        return _super.call(this, name) || this;
+    }
+    return SuperUser;
+}(User));
+var jhon = new User('Jhon');
+var paul = new SuperUser('Paul');
+console.log(jhon);
+console.log(paul);
+function userGreeting(user) {
+    if (user instanceof SuperUser)
+        console.log("Ol\u00E1 ".concat(user.name, ", deseja ver os dados do sistema?"));
+    else if (user instanceof User)
+        console.log("Ol\u00E1 ".concat(user.name));
+}
+userGreeting(jhon);
+userGreeting(paul);
--- 4_narrowing/index.ts
+++ 4_narrowing/index.ts
@@ -28,3 +28,33 @@ function operations(arr: number[], operation?: string | undefined) {
 operations([1, 2, 3], 'sum')
 operations([1, 2, 3], 'multiply')
 operations([1, 2, 3])
+
+// instance of
+
+class User {
+  name: string
+
+  constructor(name: string) {
+    this.name = name
+  }
+}
+
+class SuperUser extends User {
+  constructor(name: string) {
+    super(name)
+  }
+}
+
+const jhon = new User('Jhon')
+const paul = new SuperUser('Paul')
+
+console.log(jhon)
+console.log(paul)
+
+function userGreeting(user: object) {
+  if (user instanceof SuperUser) console.log(`Olá ${user.name}, deseja ver os dados do sistema?`)
+  else if (user instanceof User) console.log(`Olá ${user.name}`)
+}
+
+userGreeting(jhon)
+userGreeting(paul)
