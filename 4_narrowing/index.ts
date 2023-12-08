bfa7c3d1023f96f0009ad603f8b6cdd4407f627b
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-08 16:38:25 -0300

    feat: add Typeof type guard

---
+++ 4_narrowing/index.ts
@@ -0,0 +1,11 @@
+// 1 - type guard
+
+function sum(a: number | string, b: number | string) {
+  if (typeof a === 'string' && typeof b === 'string') console.log(parseFloat(a) + parseFloat(b))
+  else if (typeof a === 'number' && typeof b === 'number') console.log(a + b)
+  else console.log('Impossível realizar a soma!')
+}
+
+sum('4', '59')
+sum(12, 59)
+sum('4', 59)
