6f4beb4eb10f445b9dcd78ba328a74a70ee1990c
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-08 15:56:31 -0300

    feat: add bigint type

--- 3_avancando_em_tipos/functions/index.ts
+++ 3_avancando_em_tipos/functions/index.ts
@@ -129,10 +129,10 @@ const p = document.getElementById('some-p')
 
 console.log(p!.innerText)
 
-let n: bigint
+// let n: bigint
 
-n = 1000n
+// n = 1000n
 
-console.log(n)
-console.log(typeof n)
-console.log(n + 100n)
+// console.log(n)
+// console.log(typeof n)
+// console.log(n + 100n)
