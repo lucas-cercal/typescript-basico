03eabd4f473877074cb699a0d60649a4d48c2749
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-08 10:12:54 -0300

    feat: add boolean type

---
+++ 2_fundamentos/boolean/index.js
@@ -0,0 +1,9 @@
+var isTrue = true;
+var isFalse = false;
+function isEven(num) {
+    return num % 2 === 0;
+}
+console.log(isTrue);
+console.log(isFalse);
+console.log(isEven(4));
+console.log(isEven(5));
---
+++ 2_fundamentos/boolean/index.ts
@@ -0,0 +1,12 @@
+let isTrue: boolean = true
+let isFalse: boolean = false
+
+function isEven(num: number): boolean {
+  return num % 2 === 0
+}
+
+console.log(isTrue)
+console.log(isFalse)
+
+console.log(isEven(4))
+console.log(isEven(5))
