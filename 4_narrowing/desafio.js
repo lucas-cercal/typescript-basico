c13aea8f634859122c6a6515316c52d7d9f5a796
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-08 17:49:44 -0300

    feat: challenge concluded

---
+++ 4_narrowing/desafio.js
@@ -0,0 +1,33 @@
+/*
+  Criar uma função que recebe review dos usuários, em seguida utilizar o narrowing para receber o dado;
+  As possibilidades são boolean e number;
+  Serão enviados números de 1 a 5 (estrelas), prever uma mensagem para cada uma destas notas;
+  Ou false, que é quando o usuário não deixa uma review, prever um retorno para este caso também.
+*/
+function getReview(review) {
+    switch (review) {
+        case 1:
+            console.log("A review foi de ".concat(review));
+            break;
+        case 2:
+            console.log("A review foi de ".concat(review));
+            break;
+        case 3:
+            console.log("A review foi de ".concat(review));
+            break;
+        case 4:
+            console.log("A review foi de ".concat(review));
+            break;
+        case 5:
+            console.log("A review foi de ".concat(review));
+            break;
+        case false:
+            console.log('Sem review!');
+            break;
+    }
+}
+getReview(1);
+getReview(2);
+getReview(3);
+getReview(5);
+getReview(false);
