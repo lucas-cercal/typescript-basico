590a7550da92d3febebed94d4a7e74fb205434bb
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-08 17:19:54 -0300

    feat: operator in

--- 4_narrowing/index.ts
+++ 4_narrowing/index.ts
@@ -58,3 +58,26 @@ function userGreeting(user: object) {
 
 userGreeting(jhon)
 userGreeting(paul)
+
+// 4 - operator in
+
+class Dog {
+  name: string
+  breed: string
+
+  constructor(name: string, breed?: string) {
+    this.name = name
+    if (breed) this.breed = breed
+  }
+}
+
+const turca = new Dog('Turca')
+const bob = new Dog('Bob', 'Pastor Alemão')
+
+function showDogDetails(dog: Dog) {
+  if ('breed' in dog) console.log(`O cachorro é da raça ${dog.breed}`)
+  else console.log('O cachorro é um SRD')
+}
+
+showDogDetails(turca)
+showDogDetails(bob)
