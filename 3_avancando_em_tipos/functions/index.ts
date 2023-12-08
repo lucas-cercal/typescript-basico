cd8947d01e59675ee14c701df5ce7a3f6975256d
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-08 12:01:37 -0300

    feat: add type of parameters in the object

--- 3_avancando_em_tipos/functions/index.ts
+++ 3_avancando_em_tipos/functions/index.ts
@@ -13,3 +13,20 @@ function greeting(name: string): string {
 }
 
 console.log(greeting('Lucas'))
+
+setTimeout(function () {
+  const sallary: number = 1000
+  //console.log(sallary)
+}, 2000)
+
+function passCoordinates(coord: { x: number, y: number }) {
+  console.log('X coordinates: ' + coord.x)
+  console.log('X coordinates: ' + coord.y)
+}
+
+const objCoord = { x: 329, y: 84.2 }
+
+passCoordinates(objCoord)
+
+const pessoaObj: { name: string, surname: string } = { name: "Lucas", surname: "Cercal Fontes" }
+console.log(pessoaObj)
