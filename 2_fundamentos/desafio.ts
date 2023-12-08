d311e2a19fd9449a88c3349d63cfa1f615a45e6a
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-08 10:55:28 -0300

    feat: challenge concluded

---
+++ 2_fundamentos/desafio.ts
@@ -0,0 +1,12 @@
+/*
+  - Criar uma variável que recebe um número;
+  - Converter este número para string em uma nova variável;
+  - Esta variável de conversão deve estar tipada por inferência;
+  - Imprimir este número em uma string maior, utilizando o recurso de template string ou concatenação.
+*/
+
+let number = 28
+
+let string = number.toString()
+
+console.log(`Bianca tem ${string} anos`)
