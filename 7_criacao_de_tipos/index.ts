cf5913c06d765133d0100aa79ddb2e694a1cbff5
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-11 17:04:44 -0300

    feat: reducing accepted types in generics

--- 7_criacao_de_tipos/index.ts
+++ 7_criacao_de_tipos/index.ts
@@ -7,3 +7,16 @@ console.log(showData(5))
 console.log(showData('Testando generic'))
 console.log(showData(true))
 console.log(showData(['teste']))
+
+// 2 - constraint em generic
+function showProductName<T extends { name: string }>(obj: T) {
+  return `O nome do produto é: ${obj.name}`
+}
+
+const myObj = { name: 'Porta', colour: 'Branca' }
+const otherProduct = { name: 'Carro', wheels: 4, engine: 1.0 }
+const thirdObj = { price: 19.90, category: 'Roupa' }
+
+console.log(showProductName(myObj))
+console.log(showProductName(otherProduct))
+// console.log(showProductName(thirdObj))
