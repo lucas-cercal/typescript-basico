0b6646dd933c5973d76b23209d82c688aa47770a
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-11 17:19:13 -0300

    feat: add generics with interface

--- 7_criacao_de_tipos/index.ts
+++ 7_criacao_de_tipos/index.ts
@@ -13,10 +13,27 @@ function showProductName<T extends { name: string }>(obj: T) {
   return `O nome do produto é: ${obj.name}`
 }
 
-const myObj = { name: 'Porta', colour: 'Branca' }
+const myObj = { name: 'Porta', color: 'Branca' }
 const otherProduct = { name: 'Carro', wheels: 4, engine: 1.0 }
 const thirdObj = { price: 19.90, category: 'Roupa' }
 
 console.log(showProductName(myObj))
 console.log(showProductName(otherProduct))
 // console.log(showProductName(thirdObj))
+
+// 3 - generics com interface
+interface MyObject<T, U, Q> {
+  name: string,
+  wheels: T,
+  engine: U
+  color: Q
+}
+
+type Car = MyObject<number, number, string>
+type Pen = MyObject<boolean, boolean, string>
+
+const myCar: Car = { name: 'Fusca', wheels: 4, engine: 1.0, color: 'Branco' }
+const myPen: Pen = { name: 'Caneta BIC', wheels: false, engine: false, color: 'Azul' }
+
+console.log(myCar)
+console.log(myPen)
