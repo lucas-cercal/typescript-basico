f077fcc22e989dca3fa97d2558aae4d1333d6d44
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-11 11:59:36 -0300

    feat: add destructuring

--- 5_funcoes/index.ts
+++ 5_funcoes/index.ts
@@ -117,3 +117,14 @@ function sumAll(...n: number[]) {
 console.log(sumAll(1, 2, 3, 4, 5))
 console.log(sumAll(5, 348, 2348))
 // console.log(sumAll('teste'))
+
+// 11 - destructuring como parâmetro
+function showProductDetails({ name, price }: { name: string, price: number }): string {
+  return `O nome do produto é ${name} e ele custa R$${price}`
+}
+
+const shirt = { name: 'Camiseta', price: 49.99 }
+
+console.log(showProductDetails(shirt))
+// console.log(showProductDetails({ name: 'Teste', age: 30 }))
+// console.log(showProductDetails([1, 2]))
