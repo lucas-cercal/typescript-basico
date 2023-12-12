5e33a3b32ef12d0938805b54da1ea718dd168428
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-12 14:20:12 -0300

    feat: add this

--- 8_classes/index.ts
+++ 8_classes/index.ts
@@ -109,3 +109,24 @@ const jimmy = new Dwarf('Jimmy')
 console.log(jimmy)
 
 jimmy.greeting()
+
+// 6 - this
+class Truck {
+  model: string
+  hp: number
+
+  constructor(model: string, hp: number) {
+    this.model = model
+    this.hp = hp
+  }
+
+  showDetails() {
+    console.log(`Caminhão do modelo: ${this.model}, que tem ${this.hp} cavalos de potência`)
+  }
+}
+
+const volvo = new Truck('Volvo', 400)
+const scania = new Truck('Scania', 500)
+
+volvo.showDetails()
+scania.showDetails()
