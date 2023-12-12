f841fea5f141091d398c1a78aa1b213ca7238a20
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-12 15:05:37 -0300

    feat: add setters

--- 8_classes/index.ts
+++ 8_classes/index.ts
@@ -151,3 +151,40 @@ const lucasCercal = new Person('Lucas', 'Cercal')
 console.log(lucasCercal.name)
 
 console.log(lucasCercal.fullName)
+
+// 8 - setters
+class Coords {
+  x!: number
+  y!: number
+
+  set fillX(x: number) {
+    if (x === 0) return
+
+    this.x = x
+
+    console.log('X inserido com sucesso')
+  }
+
+  set fillY(y: number) {
+    if (y === 0) return
+
+    this.y = y
+
+    console.log('Y inserido com sucesso')
+  }
+
+  get getCoords(){
+    return `X: ${this.x} e Y: ${this.y}`
+  }
+}
+
+const myCoords = new Coords()
+
+myCoords.fillX = 15
+myCoords.fillY = 0
+
+console.log(myCoords)
+
+myCoords.fillY = 10
+
+console.log(myCoords.getCoords)
