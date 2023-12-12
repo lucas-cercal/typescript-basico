06e59f6145ce6ee9f398f504442b90c5749a88f5
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-12 11:33:39 -0300

    feat: add constructor

--- 8_classes/index.ts
+++ 8_classes/index.ts
@@ -12,3 +12,20 @@ lucas.name = 'Lucas'
 // lucas.job = 'Programmer'
 
 console.log(lucas)
+
+// 2 - constructor
+class NewUser {
+  name: string
+  age: number
+
+  constructor(name: string, age: number) {
+    this.name = name
+    this.age = age
+  }
+}
+
+const joao = new NewUser('Joao', 23)
+
+console.log(joao)
+
+// const pedro = new NewUser(12, 12)
