f53169cdd3f6059499cae7f0161a72d82ad5a4e8
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-12 10:39:44 -0300

    feat: add indexed access types

--- 7_criacao_de_tipos/index.ts
+++ 7_criacao_de_tipos/index.ts
@@ -81,3 +81,20 @@ const userName2: typeof userName = 'Pedro'
 type x = typeof userName
 
 const userName4: x = 'Tiago'
+
+// 7 - indexed access type
+type Truck = { km: number, kg: number, description: string }
+
+type Km = Truck['km']
+
+const newTruck: Truck = {
+  km: 10000,
+  kg: 5000,
+  description: 'Caminhão para pouca carga'
+}
+
+function showKm(km: Km) {
+  console.log(`O veículo tem a km de: ${km}`)
+}
+
+showKm(newTruck.km)
