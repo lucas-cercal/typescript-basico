89a0f36d8bd689aed24bbe94496d6e09566b9134
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-12 16:53:24 -0300

    feat: add private

--- 8_classes/index.ts
+++ 8_classes/index.ts
@@ -284,3 +284,36 @@ const fInstance = new F()
 fInstance.show()
 
 fInstance.showProtectedMethod()
+
+// 13 - private
+class PrivateClass {
+  private name = 'Private'
+
+  showName() {
+    return this.name
+  }
+
+  private privateMethod() {
+    console.log('Método privado')
+  }
+
+  showPrivateMethod() {
+    this.privateMethod()
+  }
+}
+
+const pObj = new PrivateClass()
+
+// console.log(pObj.name)
+
+console.log(pObj.showName())
+
+// console.log(pObj.privateMethod())
+
+pObj.showPrivateMethod()
+
+// class TestingPrivate extends PrivateClass {
+//   myMethod() {
+//     this.privateMethod()
+//   }
+// }
