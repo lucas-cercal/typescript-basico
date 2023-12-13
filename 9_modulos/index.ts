8d333e30e057c11a1b80a79cb4e303180e923d5d
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-13 09:04:31 -0300

    feat: add variable import

--- 9_modulos/index.ts
+++ 9_modulos/index.ts
@@ -1,5 +1,9 @@
-// 1 - importacao de arquivos
-
+// 1 - importacao de arquivo
 import importGreet from "./greet"
 
 importGreet()
+
+// 2 - importacao de variavel
+import { x } from "./variable"
+
+console.log(x)
