7e51864d74ce4d351089757f4723dd642571898d
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-13 16:55:08 -0300

    feat: add real method decorator

--- 10_decorators/index.ts
+++ 10_decorators/index.ts
@@ -178,3 +178,40 @@ console.log(newBook)
 console.log(newPen)
 
 console.log(newBook.createdAt)
+
+// 8 - exemplo real method decorator
+function checkIfUserPosted() {
+  return function (
+    target: Object,
+    key: string | Symbol,
+    descriptor: PropertyDescriptor
+  ) {
+    const childFunction = descriptor.value
+    console.log(childFunction)
+    descriptor.value = function (...args: any[]) {
+      if(args[1] === true) {
+        console.log('Usuário já postou!')
+        return null
+      } else {
+        return childFunction.apply(this, args)
+      }
+    }
+    return descriptor
+  }
+}
+
+class Post {
+  alreadyPosted = false
+
+  @checkIfUserPosted()
+  post(content: string, alreadyPosted: boolean) {
+    this.alreadyPosted = true
+    console.log(`Post do usuário: ${content}`)
+  }
+}
+
+const newPost = new Post()
+
+newPost.post('Meu primeiro post!', newPost.alreadyPosted)
+
+newPost.post('Meu primeiro post!', newPost.alreadyPosted)
