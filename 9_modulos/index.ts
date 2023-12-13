ce21e36bff8c2630932448c8262ddc0ff087d534
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-13 09:38:01 -0300

    feat: importing types

--- 9_modulos/index.ts
+++ 9_modulos/index.ts
@@ -26,3 +26,20 @@ console.log(name)
 import * as myNumbers from "./numbers"
 
 console.log(myNumbers)
+
+// 6 - importando tipos
+import { Human } from "./mytype"
+
+class User implements Human {
+  name: string
+  age: number
+
+  constructor(name: string, age: number) {
+    this.name = name
+    this.age = age
+  }
+}
+
+const lucas = new User('Lucas', 24)
+
+console.log(lucas)
