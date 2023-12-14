62bff5e228e2741fe2a302c99aa29a01cdfa9f4a
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-14 10:41:39 -0300

    feat: add variables

--- 11_react_com_ts/src/App.tsx
+++ 11_react_com_ts/src/App.tsx
@@ -1,7 +1,16 @@
 function App() {
+
+  // 1 - variaveis
+  const name: string = 'Lucas'
+  const age: number = 30
+  const isWorking: boolean = true
+
   return (
     <div className="App">
       <h1>Typescript com React</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && <p>Está trabalhando!</p>}
     </div>
   );
 }
