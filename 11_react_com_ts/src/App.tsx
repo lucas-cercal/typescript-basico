6761efe3e51b81c1f1ba99a299679fb438c1d687
Lucas Cercal <lucascercal.fontes@gmail.com>
2023-12-14 10:45:16 -0300

    feat: add functions

--- 11_react_com_ts/src/App.tsx
+++ 11_react_com_ts/src/App.tsx
@@ -5,12 +5,18 @@ function App() {
   const age: number = 30
   const isWorking: boolean = true
 
+  // 2 - funcoes
+  const userGreeting = (name: string): string => {
+    return `Olá, ${name}`
+  }
+
   return (
     <div className="App">
       <h1>Typescript com React</h1>
       <h2>Nome: {name}</h2>
       <p>Idade: {age}</p>
       {isWorking && <p>Está trabalhando!</p>}
+      <h3>{userGreeting(name)}</h3>
     </div>
   );
 }
